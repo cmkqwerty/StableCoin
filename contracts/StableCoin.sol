643ffb35a2c6d731dce2f06983403e4bfb16965c
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-06-25 22:16:04 +0300

    [feat] Price System Added

--- contracts/StableCoin.sol
+++ contracts/StableCoin.sol
@@ -1,29 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {ERC20} from "./ERC20.sol";
-import {DepositorCoin} from "./DepositorCoin.sol";
+import { ERC20 } from "./ERC20.sol";
+import { DepositorCoin } from "./DepositorCoin.sol";
+import { Oracle } from "./Oracle.sol";
 
 contract StableCoin is ERC20 {
     DepositorCoin public depositorCoin;
-    uint256 private constant ETH_IN_USD_PRICE = 2000;
-    uint256 public feeRatePer; 
 
-    constructor(uint256 _feeRatePer) ERC20("StableCoin", "STC") {
+    uint256 public feeRatePer; 
+    Oracle public oracle;
+
+    constructor(uint256 _feeRatePer, Oracle _oracle) ERC20("StableCoin", "STC") {
         feeRatePer = _feeRatePer;
+        oracle = _oracle;
     }
 
     function mint() external payable {
         uint256 fee = _getFee(msg.value);
         uint256 remainingEth = msg.value - fee;
-        uint256 mintStableCoinAmount = remainingEth * ETH_IN_USD_PRICE;
+        uint256 mintStableCoinAmount = remainingEth * oracle.getPrice();
         _mint(msg.sender, mintStableCoinAmount);
     }
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
 
-        uint256 ethBack = amount / ETH_IN_USD_PRICE;
+        uint256 ethBack = amount / oracle.getPrice();
         uint256 fee = _getFee(ethBack);
         uint256 ethRemainingValue = ethBack - fee;
 
